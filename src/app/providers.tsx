5909cae0ed4cbbd54a2a87c18fe36d4525ed42a1
devthigas <thi.deusefiel@gmail.com>
2024-07-10 16:45:52 -0300

    chore: estilos globais com style components

---
+++ src/app/providers.tsx
@@ -0,0 +1,11 @@
+import React, { PropsWithChildren } from 'react'
+import GlobalStyles from '@/styles/global'
+
+export default function Providers({ children }: PropsWithChildren) {
+  return (
+    <>
+      <GlobalStyles />
+      {children}
+    </>
+  )
+}
